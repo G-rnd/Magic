30278c21c988c24683106e1fe64b7b7cf45d8e89
G-rnd <76098938+G-rnd@users.noreply.github.com>
2022-01-09 15:04:27 +0100

    Add files via upload

---
+++ includes/Vector.hpp
@@ -0,0 +1,33 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
+#include <vector>
+#include <memory>
+
+template <typename E>
+class Vector {
+    private:
+        std::vector<std::unique_ptr<E> >    m_vector;
+
+    public:
+        Vector();
+        ~Vector();
+
+        template<typename T>
+        void                                add(T e);
+        template<typename T>
+        void                                push_back(T e);
+        void                                pop_back();
+        void                                clear();
+        E&                                  get(int index) const;
+        void                                remove(int index);
+        int                                 size();
+        
+        E&                                  operator[] (int index) const;
+        /*
+        const E*                            begin() const;
+        const E*                            end() const;
+*/
+};
+
+#endif
